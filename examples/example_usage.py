e8fb25b2925b7a418f4ae0914cf13ec789394e35
Kostya <k.kornilov1015@gmail.com>
2025-01-04 14:10:57 +0300

    Update example usage to include output directory for DEM and slope files

--- examples/example_usage.py
+++ examples/example_usage.py
@@ -1,16 +1,19 @@
 from src.data_download import initialize_gee, download_dem
 from src.calculations import calculate_slope
+import os
 
 initialize_gee(project='projectomela')
 
 # Определим координаты и названия файлов
 lat, lon = 55.600, 37.172
 buffer = 0.05
-dem_path = "srtm.tif"
-slope_path = "slope.tif"
+dem_file_name = "srtm.tif"
+slope_file_name = "slope.tif"
+directory = "example_output"
 
 # Загрузим DEM
-download_dem(lat, lon, buffer, dem_path)
-
+download_dem(lat, lon, buffer, dem_file_name, directory)
+dem_path = os.path.join(directory, dem_file_name)
+slope_path = os.path.join(directory, slope_file_name)
 # Рассчитаем уклон
 calculate_slope(dem_path, slope_path)
