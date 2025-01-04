8cfd9748d2b1d0bfec48d53dc164bc0125c0ec6a
Kostya <k.kornilov1015@gmail.com>
2025-01-04 15:18:38 +0300

    Update plotting function to adjust figure size and apply terrain colormap for DEM visualization

--- src/plotting.py
+++ src/plotting.py
@@ -3,10 +3,10 @@ import rioxarray
 
 def plot_dem_and_slope_files(dem_xarray, slope_xarray):
     """Построить графики для DEM и уклона на одном графике."""
-    fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
     
     # Plot DEM
-    dem_xarray.plot(ax=axes[0])
+    dem_xarray.plot(ax=axes[0], cmap='terrain')
     axes[0].set_title('Digital Elevation Model (DEM)')
     
     # Plot Slope
