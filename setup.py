4ed82dcd1cffe8ec36365fddab27ab80f26f24b7
Konstantin Kornilov <113131140+kostkornilov@users.noreply.github.com>
2025-01-12 22:44:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dem_api',
+    name='dem_features_extractor',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
